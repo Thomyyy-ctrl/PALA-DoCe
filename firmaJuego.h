37195fb4fe6e428010bf560a9abd093dd8622a88
Onlylaion <marcosleonrodriguez2020@gmail.com>
2025-06-02 00:42:22 -0300

    Update firmaJuego.h

--- firmaJuego.h
+++ firmaJuego.h
@@ -1,9 +1,9 @@
 #ifndef FIRMAJUEGO_H_INCLUDED
 #define FIRMAJUEGO_H_INCLUDED
 #define IA_DIFICIL 2
-#define NOMBRE_IA_DIFICIL "IA DIFICIL"
-#define TIPO_ESPEJO 3
-#define TIPO_REPETIR_TURNO 4
+///#define NOMBRE_IA_DIFICIL "IA DIFICIL"
+///#define TIPO_ESPEJO 3
+///#define TIPO_REPETIR_TURNO 4
 #include"firmaTdaLista.h"
 
 typedef struct
@@ -23,6 +23,6 @@ typedef struct
 } tPlayer;
 
 
-tCarta* IADificil(const tPila *historialJugadas, const tPlayer *humano, const tPlayer*IA, tCarta *mano);
+///tCarta* IADificil(const tPila *historialJugadas, const tPlayer *humano, const tPlayer*IA, tCarta *mano);
 int crearArchivo(const char*path);
 #endif // FIRMAJUEGO_H_INCLUDED
